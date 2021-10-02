72f0f05b825d660932a382185e9ff47f656bdee6
Hammurabi <koberstine@hotmail.com>
2021-10-01 20:48:24 -0400

    created employee class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,18 @@
+const Employee = require('../lib/Employee.js');
+
+test('creates a employee object', () => {
+  const employee = new Employee('Test Employee', '001', 'test@test.com');
+
+  expect(employee.name).toBe('Test Employee');
+  expect(employee.id).toBe('001');
+  expect(employee.email).toBe('test@test.com')
+});
+
+test("gets employee's info as an object", () => {
+  const employee = new Employee('Test Employee', '001', 'test@test.com');
+  
+  expect(employee.getName()).toBe(employee.name);
+  expect(employee.getId()).toBe(employee.id);
+  expect(employee.getEmail()).toBe(employee.email);
+  expect(employee.getRole()).toBe('Employee');
+});
